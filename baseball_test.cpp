060ca1eef1a6dea2b11702a096fa3ef70516f80a
badgoofyman <badgoofy@gmail.com>
2026-05-28 13:38:06 +0900

    [refactoring] 2

--- baseball_test.cpp
+++ baseball_test.cpp
@@ -1,14 +1,24 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
-}
+	void assertIllegalArgument(string geussNumber) {
+		try {
+			game.guess(geussNumber);
+			FAIL();
+		}
+		catch (exception e) {
+			// PASS();
+		}
+	}
+};
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
 
 int main() {
